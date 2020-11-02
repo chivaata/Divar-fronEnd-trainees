5ec2dfe2132dfdb1a5a64dec536baaf6e91f03cd
chivaata <Chivatavakol@gmail.com>
2020-11-02 12:47:22 +0330

    SubWidget component changed to functional component

--- src/subWidget.js
+++ src/subWidget.js
@@ -1,20 +1,15 @@
 import React from "react";
 
-// KOLAN INJA
-class SubWidget extends React.Component {
-  render() {
-    const { item } = this.props;
-
-    return (
-      <div className="option">
-        {item.data.action.payload.widget_list.map((list) => {
-          if (list.widget_type === "DESCRIPTION_ROW") {
-            return <div>{list.data.text}</div>;
-          }
-        })}
-      </div>
-    );
-  }
+function SubWidget({ item }) {
+  return (
+    <div className="option">
+      {item.data.action.payload.widget_list.map((list) => {
+        if (list.widget_type === "DESCRIPTION_ROW") {
+          return <div>{list.data.text}</div>;
+        }
+      })}
+    </div>
+  );
 }
 
 export default SubWidget;
