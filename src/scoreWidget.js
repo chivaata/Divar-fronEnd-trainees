02c9948a82d2139ee31a38c57c5eae59d45ae3ae
chivaata <Chivatavakol@gmail.com>
2020-11-02 16:41:28 +0330

    Score component changed to function component.

--- src/scoreWidget.js
+++ src/scoreWidget.js
@@ -1,66 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./widgets.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./fontawesomeLibrary.js";
 import Percent from "./percent";
 import SubWidget from "./subWidget";
 
-class Score extends React.Component {
-  state = {
-    isOptionOpen: false,
-    isBodyOpen: false,
+function Score({ item, logo, title, percent }) {
+  const [isOptionOpen, setIsOptionOpen] = useState(false);
+  const [isBodyOpen, setIsBodyOpen] = useState(false);
+
+  const showOption = () => {
+    setIsOptionOpen(!isOptionOpen);
   };
 
-  showOption = () => {
-    this.setState({ isOptionOpen: !this.state.isOptionOpen });
+  const showBody = () => {
+    setIsBodyOpen(!isBodyOpen);
   };
 
-  showBody = () => {
-    this.setState({ isBodyOpen: !this.state.isBodyOpen });
-  };
-
-  render() {
-    const { item, logo, title, percent } = this.props;
-    const { isBodyOpen, isOptionOpen } = this.state;
-    return (
-      <>
+  return (
+    <>
+      <div
+        className="score"
+        role="button"
+        onClick={(e) => {
+          showOption();
+          console.log("click");
+        }}
+      >
+        <div>
+          <img className="image-logo" src={logo} alt="logo" />
+        </div>
         <div
-          className="score"
-          role="button"
           onClick={(e) => {
-            this.showOption();
-            console.log("click");
+            if (title === "بدنه") {
+              showBody();
+            }
           }}
         >
-          <div>
-            <img className="image-logo" src={logo} alt="logo" />
-          </div>
-          <div
-            onClick={(e) => {
-              if (title === "بدنه") {
-                this.showBody();
-              }
-            }}
-          >
-            {title}
-          </div>
-          <div>
-            <Percent percent={percent} />
-          </div>
-          <div>
-            <FontAwesomeIcon
-              icon="angle-left"
-              style={{ color: "#898c8a", float: "left" }}
-            />
-          </div>
+          {title}
         </div>
-        <hr className="divider" />
-        {(isBodyOpen || isOptionOpen) && (
-          <SubWidget title={title} item={item} />
-        )}
-      </>
-    );
-  }
+        <div>
+          <Percent percent={percent} />
+        </div>
+        <div>
+          <FontAwesomeIcon
+            icon="angle-left"
+            style={{ color: "#898c8a", float: "left" }}
+          />
+        </div>
+      </div>
+      <hr className="divider" />
+      {(isBodyOpen || isOptionOpen) && <SubWidget title={title} item={item} />}
+    </>
+  );
 }
 
 export default Score;
